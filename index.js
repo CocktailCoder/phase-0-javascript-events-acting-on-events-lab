4e0cfa43050db550df564ebc184d6245b79a97da
cocktailcoder <iokreddit@protonmail.com>
2021-06-06 15:39:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var dodger = document.getElementById('dodger');
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left - 5}px`
   }
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10)
   if (left < 360) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left = `${left + 5}px`
   }
 }
 
